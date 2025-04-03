a20dd105e12feb1f7ecb0fee0c25721eb331a226
Muneer Ahmad <shoaib786asme@gmail.com>
2025-04-03 16:22:04 +0530

    first commit

---
+++ src/test/java/com/java/day0/BasicCheck.java
@@ -0,0 +1,13 @@
+package com.java.day0;
+
+import org.testng.annotations.Test;
+
+public class BasicCheck {
+	
+	@Test
+	public void method1(){
+		
+		System.out.println("######Hello world this is simple code#######");
+	}
+
+}
